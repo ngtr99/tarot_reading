2b65b3c13a918a2fa009e9fa2758bcd3530e3424
Nguyet minh Trang <minhnguyet12923@gmail.com>
2025-03-01 16:26:53 -0800

    Update settings.py

--- tarot_reading/settings.py
+++ tarot_reading/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-is)@xt@2%uy4srl7in$5_9rm@h!^=w#cl6#*4%*5yqbml!+ofs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1'']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 # Application definition
 
